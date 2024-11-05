630f185ac8da7be419a31ea56fd6bd34f0a4cdb5
raviarsh01 <raviarsh786@gmail.com>
2024-11-05 23:52:22 +0530

    Created model and controller for new order

---
+++ backend/controller/orders-contoller.js
@@ -0,0 +1,56 @@
+const Orders = require("../models/order-model");
+
+const CreateOrder = async (req, res) => {
+  const { customerID, deliveryAddress, cartData, totalAmount } = req.body;
+  if (!customerID || !deliveryAddress || !cartData.length > 0 || !totalAmount) {
+    return res.status(400).json({ errors: "Please fill all fields" });
+  }
+
+  try {
+    const newOrder = new Orders({
+      customerID,
+      deliveryAddress,
+      cartData,
+      totalAmount,
+    });
+    await newOrder.save();
+    res.status(201).json({ success: true, message: "Order created" });
+  } catch (error) {
+    res.status(500).json({ success: "false", error: "Internal Server Error" });
+  }
+};
+const GetOrders = async (req, res) => {
+  const { customerId } = req.params;
+  console.log("api hit");
+  if (!customerId) {
+    return res.status(400).json({ errors: "Field is required" });
+  }
+  try {
+    const data = await Orders.find({ customerID: customerId })
+      .populate({
+        path: "customerID",
+        select: "firstName lastName email",
+      })
+      .populate({
+        path: "cartData.itemID",
+      })
+      .populate({
+        path: "deliveryAddress",
+        select: "-_id -customerID -createdAt -updatedAt -__v",
+      });
+    // .populate(
+    //   "customerID",
+    //   "firstName lastName email"
+    // );
+    res.status(200).json({
+      success: "Get successfully",
+      page: null,
+      response: data,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { CreateOrder, GetOrders };
