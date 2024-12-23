e33b56cb71e1d7a38fb28b1ba19871cb11bc7b7d
raviarsh01 <raviarsh786@gmail.com>
2024-12-23 23:54:54 +0530

    Added user profile update and change password controller

--- backend/routes/address-routes.js
+++ backend/routes/address-routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const {
-  AddAddress,
-  GetAddress,
-} = require("../controller/address-contoller");
+const { AddAddress, GetAddress } = require("../controller/address-contoller");
+const verifyToken = require("../middleware/verify-token");
+
 const router = express.Router();
 
-router.post("/add-address", AddAddress);
-router.get("/get-address/:customerId", GetAddress);
+router.post("/add-address", verifyToken, AddAddress);
+router.get("/get-address", verifyToken, GetAddress);
 
 module.exports = router;
